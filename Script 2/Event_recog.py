17d540823d2e9cd8f60e370b9c03373214c0b85f
Cesar Villegas <99771832+cesarvillegase@users.noreply.github.com>
2023-09-08 01:11:53 -0600

    Update Event recognizer
    Implementation of an extraction of the variable used in the twitter bot to automatize the upload of tweets with the variable in the moment of the event.
    
    Co-Authored-By: Kevin Mejia <101744671+kevinsky29@users.noreply.github.com>
    Co-Authored-By: J.A.R.V.S. <joab.ramos27@gmail.com>

--- Script 2/Event_recog.py
+++ Script 2/Event_recog.py
@@ -21,14 +21,59 @@ csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['Frame', 'Extracted Text', 'Best Match'])
 
 # Keywords to recognize and train
-keywords = ['was defeated', 'enemy', 'fainted', 'gotcha', 'Gotcha']
+keywords = ['Enemy', 'Gotcha']
+
+pokemon_names_keywords = [
+    'Chikorita', 'Bayleef', 'Meganium', 'Cyndaquil', 'Quilava', 'Typhlosion',
+    'Totodile', 'Croconaw', 'Feraligatr', 'Sentret', 'Furret', 'Hoothoot',
+    'Noctowl', 'Ledyba', 'Ledian', 'Spinarak', 'Ariados', 'Crobat',
+    'Chinchou', 'Lanturn', 'Pichu', 'Cleffa', 'Igglybuff', 'Togepi',
+    'Togetic', 'Natu', 'Xatu', 'Mareep', 'Flaaffy', 'Ampharos',
+    'Bellossom', 'Marill', 'Azumarill', 'Sudowoodo', 'Politoed', 'Hoppip',
+    'Skiploom', 'Jumpluff', 'Aipom', 'Sunkern', 'Sunflora', 'Yanma',
+    'Wooper', 'Quagsire', 'Espeon', 'Umbreon', 'Murkrow', 'Slowking',
+    'Misdreavous', 'Unown', 'Wobbuffet', 'Girafarig', 'Pineco', 'Forretress',
+    'Dunsparce', 'Gligar', 'Steelix', 'Snubbull', 'Granbull', 'Qwilfish',
+    'Scizor', 'Shuckle', 'Heracross', 'Sneasel', 'Teddiursa', 'Ursaring',
+    'Slugma', 'Magcargo', 'Swinub', 'Piloswine', 'Corsola', 'Remoraid',
+    'Octillery', 'Delibird', 'Mantine', 'Skarmory', 'Houndour', 'Houndoom',
+    'Kingdra', 'Phanpy', 'Donphan', 'Porygon2', 'Stantler', 'Smeargle',
+    'Tyrogue', 'Hitmontop', 'Smoochum', 'Elekid', 'Magby', 'Miltank',
+    'Blissey', 'Raikou', 'Entei', 'Suicune', 'Larvitar', 'Pupitar',
+    'Tyranitar', 'Lugia', 'Ho-oh', 'Celebi', 'Treecko', 'Grovyle',
+    'Sceptile', 'Torchic', 'Combusken', 'Blaziken', 'Mudkip', 'Marshtomp',
+    'Swampert', 'Poochyena', 'Mightyena', 'Zigzagoon', 'Linoone', 'Wurmple',
+    'Silcoon', 'Beautifly', 'Cascoon', 'Dustox', 'Lotad', 'Lombre',
+    'Ludicolo', 'Seedot', 'Nuzleaf', 'Shiftry', 'Taillow', 'Swellow',
+    'Wingull', 'Pelipper', 'Ralts', 'Kirlia', 'Gardevoir', 'Surskit',
+    'Masquerain', 'Shroomish', 'Breloom', 'Slakoth', 'Vigoroth', 'Slaking',
+    'Nincada', 'Ninjask', 'Shedinja', 'Whismur', 'Loudred', 'Exploud',
+    'Makuhita', 'Hariyama', 'Azurill', 'Nosepass', 'Skitty', 'Delcatty',
+    'Sableye', 'Mawile', 'Aron', 'Lairon', 'Aggron', 'Meditite',
+    'Medicham', 'Electrike', 'Manectric', 'Plusle', 'Minun', 'Volbeat',
+    'Illumise', 'Roselia', 'Gulpin', 'Swalot', 'Carvanha', 'Sharpedo',
+    'Wailmer', 'Wailord', 'Numel', 'Camerupt', 'Torkoal', 'Spoink',
+    'Grumpig', 'Spinda', 'Trapinch', 'Vibrava', 'Flygon', 'Cacnea',
+    'Cacturne', 'Swablu', 'Altaria', 'Zangoose', 'Seviper', 'Lunatone',
+    'Solrock', 'Barboach', 'Whiscash', 'Corphish', 'Crawdaunt', 'Baltoy',
+    'Claydol', 'Lileep', 'Cradily', 'Anorith', 'Armaldo', 'Feebas',
+    'Milotic', 'Castform', 'Kecleon', 'Shuppet', 'Banette', 'Duskull',
+    'Dusclops', 'Tropius', 'Chimecho', 'Absol', 'Wynaut', 'Snorunt',
+    'Glalie', 'Spheal', 'Sealeo', 'Walrein', 'Clamperl', 'Huntail',
+    'Gorebyss', 'Relicanth', 'Luvdisc', 'Bagon', 'Shelgon', 'Salamence',
+    'Beldum', 'Metang', 'Metagross', 'Regirock', 'Regice', 'Registeel',
+    'Latias', 'Latios', 'Kyogre', 'Groudon', 'Rayquaza', 'Jirachi',
+    'Deoxys', 'Pidgey', 'Rattata'
+]
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 def update_text_and_frame():
-
+    
     global frame_counter
+    pokemon_found = None
+    
     while True:
         left, top, width, height = game_window.left, game_window.top, game_window.width, game_window.height
         screenshot = np.array(sct.grab({"left": left, "top": top, "width": width, "height": height}))
@@ -44,15 +89,22 @@ def update_text_and_frame():
         csv_writer.writerow([frame_counter, extracted_text, best_match])
 
         # Take a screenshot when a certain keyword appears on the screen
-        if any(keyword in extracted_text for keyword in keywords):
-            #cv2.imwrite(f'screenshot_{frame_counter}.png', frame)
-            screenshot_path = 'Detect_object\Script 2\screenshot.png'
-            cv2.imwrite(screenshot_path, frame)
-            twt_bot = Twtbot()
-            twt_bot.run(screenshot_path)  # Run Twtbot when keyword is detected
+        if any(keyword.lower() in extracted_text.lower() for keyword in keywords):
+            # Find the Pokémon name in the extracted text
+            for pokemon in pokemon_names_keywords:
+                if pokemon.lower() in extracted_text.lower():
+                    pokemon_found = pokemon
+                    break
+
+            if pokemon_found:
+                screenshot_path = 'Detect_object\Script 2\screenshot.png'
+                rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                cv2.imwrite(screenshot_path, rgb_frame)
+                twt_bot = Twtbot()
+                twt_bot.run(screenshot_path, pokemon_found)  # Run Twtbot when keyword is detected
 
         frame_counter += 1
-        time.sleep(1)  # Optional pause to control capture frequency
+        time.sleep(1) 
 
 # Create the MSS object for screen capture
 with mss.mss() as sct:
