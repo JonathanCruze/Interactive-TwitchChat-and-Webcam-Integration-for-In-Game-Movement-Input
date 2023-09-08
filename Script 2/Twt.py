b68e9d365e232216a86781fe773af70c316457ff
Cesar Villegas <99771832+cesarvillegase@users.noreply.github.com>
2023-09-08 01:17:31 -0600

    Update of the Twitter bot
    Use of the saved variable in the event recognizer to create the automatized tweets of certain events.

--- Script 2/Twt.py
+++ Script 2/Twt.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class Twtbot:
     def __init__(self):
         self.driver = webdriver.Chrome()
-
+    
     def login(self, username, password):
         # Open Twitter
         self.driver.get("https://twitter.com/search")
@@ -26,42 +26,41 @@ class Twtbot:
         password_input.send_keys(Keys.RETURN)
 
         time.sleep(5)
-
+    
     def post(self, tweet_content, media_path):
-
+        
         post_link = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="SideNav_NewTweet_Button"]')
         post_link.click()
-
+        
         tweet_box = WebDriverWait(self.driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="tweetTextarea_0"]'))
         )
-
+        
         time.sleep(5)
-
+        
         tweet_box.send_keys(tweet_content)
-
+        
         time.sleep(2)
-
+        
         ############# UPLOAD IMAGE ########
-
+        
         media_button = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="fileInput"]')
         media_button.send_keys(os.path.abspath(media_path))
-
+        
         time.sleep(5)   
-
+        
         tweet_button = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="tweetButton"]')
         tweet_button.click()
-
+        
         time.sleep(5)
-
-    def run(self, media_path):
-
+        
+    def run(self, media_path, pokemon_found):
+        
         username = 'twitter_username'  
         password = 'twitter_password'
-        pokemon = 'Chorizard'
         media_path = "Detect_object\Script 2\screenshot.png"
-        tweet_content = f"Hemos encontrado a: {pokemon}" 
-
+        tweet_content = f"Hemos encontrado a: {pokemon_found}" 
+        
         self.login(username, password) 
         self.post(tweet_content, media_path)
         self.driver.quit()
